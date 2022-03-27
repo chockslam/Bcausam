072f8f0eedf5c5e675ac1fa79d4a0665f06fd262
FrankieStalker <frankiestalker@gmail.com>
2022-03-27 13:08:36 +0100

    query of classcodes done

---
+++ wp-content/themes/astra/page-db-classcode-query.php
@@ -0,0 +1,55 @@
+<?php
+/**
+
+Template Name: Classcode query PHP
+
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Astra
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header(); ?>
+
+<?php if ( astra_page_layout() == 'left-sidebar' ) : ?>
+
+	<?php get_sidebar(); ?>
+
+<?php endif ?>
+
+	<div id="primary" <?php astra_primary_class(); ?>>
+
+		<?php astra_primary_content_top(); ?>
+
+		
+		<?php
+			//FRANKIE'S TESTING CODE
+			require 'db-classcode-query.php';
+			//$classCodes = CopyDatabase();
+			//$contents = QueryClassCode("101;102;109;112",$classCodes);
+			$contents = CopyDatabase();
+			print("<pre>".print_r($contents,true)."</pre>");
+		?>
+
+		<?php astra_primary_content_bottom(); ?>
+
+	</div><!-- #primary -->
+
+<?php if ( astra_page_layout() == 'right-sidebar' ) : ?>
+
+	<?php get_sidebar(); ?>
+
+<?php endif ?>
+
+<?php get_footer(); ?>
