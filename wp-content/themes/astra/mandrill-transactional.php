0028d4b12b9ec28cc71cb5ac3a1232b3d9882877
BenP31 <ben.c.palmer31@outlook.com>
2022-03-28 13:49:36 +0100

    Fixed test env, added test function

--- wp-content/themes/astra/mandrill-transactional.php
+++ wp-content/themes/astra/mandrill-transactional.php
@@ -1,32 +1,56 @@
 <?php
 
-    function test() {
-        $
-    }
+    DEFINE('MC_API_KEY',"cQCVN00C9F8Jr24yCYKxZQ");
 
-    function postMessage($messageContent) {
-        $arr = array(
-            'body'  =>  $messageContent
-        );
-
-        $response = wp_remote_post("https://mandrillapp.com/api/1.0/message/send-template", $arr);
-        return $response
-    }
-
-
-    function formatMessage($message) {
+    function test_connection() {
+        $url = "https://mandrillapp.com/api/1.0/users/ping";
         $content = array(
-            'key' => $MC_API_KEY,
-            'template' => $MC_TEMPLATE_NAME,
-            'template_content' => template_content,
-            'message' => message_content,
-            'async' => false,
-            'ip_pool' => ,
-            'send_at' => 
-        )
+            "key" => MC_API_KEY,
+        );
+        $content = json_encode($content);
+        $q = wp_remote_post($url, array(
+            'method' => 'POST',
+            'body' => $content,
+            'headers' => array(
+                'Content-Type' => 'application/json'
+            )
+        ));
+        $response = wp_remote_retrieve_body($q);
+        $response = json_decode($response, true);
+        if ($response == "PONG!") {
+            return true;
+        } else {
+            return false;
+        }
     }
 
+    // function postMessage($messageContent) {
+    //     $arr = array(
+    //         'body'  =>  $messageContent
+    //     );
 
-    function 
+    //     $response = wp_remote_post("https://mandrillapp.com/api/1.0/message/send-template", array(
+    //         'method' => 'POST',
+    //         'body' => $arr,
+    //         'headers' => array(
+    //             'Content-Type' => 'application/json'
+    //         )
+    //     ));
+    // }
+
+
+    // function formatMessage($message) {
+    //     $content = array(
+    //         'key' => $MC_API_KEY,
+    //         'template' => $MC_TEMPLATE_NAME,
+    //         'template_content' => template_content,
+    //         'message' => message_content,
+    //         'async' => false,
+    //         'ip_pool' => 'Main Pool',
+    //         'send_at' => 'test'
+    //     );
+    // }
+
+
 
 ?>
